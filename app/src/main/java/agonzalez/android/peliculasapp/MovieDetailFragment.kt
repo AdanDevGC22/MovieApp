ffea13389daf42c50479031be4f51eb4a93e177d
Adan Gonzalez <adangonzalez@wposs.com>
2022-09-22 16:57:02 -0500

    listo para empezar a aplicar la arquitectura

--- app/src/main/java/agonzalez/android/peliculasapp/MovieDetailFragment.kt
+++ app/src/main/java/agonzalez/android/peliculasapp/MovieDetailFragment.kt
@@ -1,5 +1,6 @@
 package agonzalez.android.peliculasapp
 
+import agonzalez.android.peliculasapp.databinding.FragmentMovieDetailBinding
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,9 +8,11 @@ import androidx.fragment.app.Fragment
 
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+    private lateinit var binding: FragmentMovieDetailBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 
 }
